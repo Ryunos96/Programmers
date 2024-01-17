0d50a9c680270771802e833da1655832e47e249a
Ryunos96 <70890073+Ryunos96@users.noreply.github.com>
2024-01-17 09:54:28 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    s_ = str(s)
+    while True:
+        leng = len(s_)
+        if leng == 2:
+            ''.join(s_)
+            return s_
+        if leng == 1:
+            ''.join(s_)
+            return s_
+        s_ = s_[1:-1]
