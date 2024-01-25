446aa8a7e49a6b41d4b0099a15af37e879853881
Ryunos96 <70890073+Ryunos96@users.noreply.github.com>
2024-01-25 09:09:25 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    if len(num_list) > 10:
+        for i in range(len(num_list)):
+            answer += num_list[i]
+    elif len(num_list) <= 10:
+        answer = 1
+        for i in range(len(num_list)):
+            answer *= num_list[i]
+    return answer
